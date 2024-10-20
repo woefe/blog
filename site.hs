748b052d89f4f0ddb0f7ec4240ac23f82adfa301
Wolfgang Popp <mail@wolfgang-popp.de>
2024-10-20 12:50:27 +0200

    Autoformat site.hs with ormolu

--- site.hs
+++ site.hs
@@ -1,96 +1,99 @@
 ----------------------------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Hakyll
+
+import Hakyll
 
 ----------------------------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith config $ do
-    match "images/*" $ do
-        route idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    match (fromList ["about.md", "contact.md"]) $ do
-        route $ setExtension "html"
-        compile
-            $   pandocCompiler
-            >>= loadAndApplyTemplate "templates/page.html"    defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match (fromList ["about.md", "contact.md"]) $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/page.html" defaultContext
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile
-            $   pandocCompiler
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts)
-                        <> constField "title" "Archive"
-                        <> defaultContext
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              <> constField "title" "Archive"
+              <> defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/page.html"    archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/page.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
+  create ["index.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              <> constField "is_index" "true"
+              <> defaultContext
 
-    create ["index.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts)
-                        <> constField "is_index" "true"
-                        <> defaultContext
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/teaser-post-list.html" indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/teaser-post-list.html" indexCtx
-                >>= loadAndApplyTemplate "templates/default.html"          indexCtx
-                >>= relativizeUrls
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = postCtx <> bodyField "description"
 
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx <> bodyField "description"
+      posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+      renderAtom myFeedConfiguration feedCtx posts
 
-            posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
-            renderAtom myFeedConfiguration feedCtx posts
-
-    match "templates/*" $ compile templateBodyCompiler
-
+  match "templates/*" $ compile templateBodyCompiler
 
 ----------------------------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y"
-        <> teaserField "teaser" "content"
-        <> snapshotField "content" "content"
-        <> defaultContext
+  dateField "date" "%B %e, %Y"
+    <> teaserField "teaser" "content"
+    <> snapshotField "content" "content"
+    <> defaultContext
 
 snapshotField :: String -> Snapshot -> Context String
 snapshotField fieldName snapshot = field fieldName createVal
-    where createVal item = itemBody <$> loadSnapshot (itemIdentifier item) snapshot
+  where
+    createVal item = itemBody <$> loadSnapshot (itemIdentifier item) snapshot
 
 myFeedConfiguration :: FeedConfiguration
-myFeedConfiguration = FeedConfiguration
-    { feedTitle       = "Woefe's Blog"
-    , feedDescription = ""
-    , feedAuthorName  = "woefe"
-    , feedAuthorEmail = ""
-    , feedRoot        = "https://blog.woefe.com"
+myFeedConfiguration =
+  FeedConfiguration
+    { feedTitle = "Woefe's Blog",
+      feedDescription = "",
+      feedAuthorName = "woefe",
+      feedAuthorEmail = "",
+      feedRoot = "https://blog.woefe.com"
     }
 
 config :: Configuration
-config = defaultConfiguration { destinationDirectory = "docs" }
+config = defaultConfiguration {destinationDirectory = "docs"}
+
+-- vim: expandtab tabstop=2 shiftwidth=2
